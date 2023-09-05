26e79b19ef60191a4eaefabdecad57430b9ba10d
pocom311121314 <pocom311121314@gmail.com>
2023-09-06 04:31:49 +0530

    first commit

---
+++ serviceWorker.js
@@ -0,0 +1,19 @@
+const dc = "dcshopv3"
+const assets = [
+  "/",
+  "/index-mobile.html",
+]
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
